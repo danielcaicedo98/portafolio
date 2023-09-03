ddaeed12510b6647ef7c17380636b048c0ed40c5
danielcaicedo98 <daniel.andres.caicedo@correounivalle.edu.co>
2023-09-02 21:45:02 -0500

    Implementation: materials

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,11 +6,13 @@ const Experience = (props) => {
 
     const boxRef = useRef();
     const sphereRef = useRef();
+   
     useFrame((state, delta) => {
         const time = state.clock.getElapsedTime();
         const positionY = Math.sin(time);
         const positionX = Math.cos(time);
 
+        
         // Aplica la posición al objeto
         boxRef.current.position.y = positionY;
         sphereRef.current.position.x = positionX;
@@ -24,29 +26,40 @@ const Experience = (props) => {
             <ambientLight intensity={0.5} />
             <directionalLight position={[4, 6, 8]} intensity={2} />
 
-
-            <mesh ref={boxRef} position={[-4, 0, 2]}>
+            <mesh  ref={boxRef} position={[-4, 0, 0]}>
                 <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color="yellow" /> 
+                <meshPhysicalMaterial color="yellow" /> 
             </mesh>
+
+            
+            
+
+            {/* <mesh  position={[0, 2, 0]}>
+                <coneGeometry args={[1, 1.5, 30]} />
+                <meshToonMaterial color="red" /> 
+            </mesh> */}
+
             <mesh ref={sphereRef} position={[0, 0, 0]}>
                 <sphereGeometry args={[1, 32, 32]} /> {/* El primer argumento es el radio, los siguientes dos son la cantidad de segmentos horizontal y vertical */}
-                <meshStandardMaterial color="red" />                
+                {
+                //<meshLambertMaterial color="red" />  
+                <meshToonMaterial color="red" />    
+                }        
+                    
             </mesh>
-
             <mesh position={[4, 0, 0]}>
                 <torusGeometry args={[0.8, 0.4, 16, 100]} /> {/* Los argumentos son: radio del toro, grosor del toro, segmentos radiales y segmentos tubulares */}
-                <meshStandardMaterial color="green" />
-            </mesh>
+                <meshPhongMaterial color="green" />
+           </mesh>
             <mesh position={[0, -3, 0]}>
                 <coneGeometry args={[1, 3, 30]} /> {/* Los argumentos son: radio de la base, altura y número de segmentos */}
-                <meshStandardMaterial color="blue" />
-            </mesh>
+                <meshMatcapMaterial color="blue" />
+            </mesh>        
 
       </>
     );
 
-   
+
 
 
 }
